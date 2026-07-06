4795ef5ac7708c57b2e9f4e7270ea60e8d2c060f
bhuvaneshwarguttula <bhuvaneshwarguttula@gmail.com>
2026-07-06 22:11:44 +0530

    Static and deynamic prompts

---
+++ LangChain_Prompts/prompt_ui.py
@@ -0,0 +1,77 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+# Load environment variables
+load_dotenv()
+
+# Initialize the Gemini model
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+)
+
+# # Make a call
+# response = llm.invoke("Whta is the captial of India")
+
+# print(response.content)
+
+st.header("Research Tool")
+
+############ Static Prompts #####################
+# user_input = st.text_input('Ask your question')
+
+# if st.button('Summarize'):
+#     response = llm.invoke(user_input)
+#     st.text(response.content)
+
+
+######### Dynamic Prompts #########################
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+## Template
+
+template = PromptTemplate(
+    template="""
+Please summarize the research paper titled "{paper_input}" with the following specifications:
+Explanation Style: "{style_input}"
+Explanantion Length: "{length_input}"
+1. Mathematical Details:
+    - Include relevant mathematical euations if present in the paper.
+    - Explain the mathematical concepts using the simple, intuitive code snippets where applicable.
+2. Analogies:
+    - Use relatable analogies to simplify complex ideas.
+If certain information is not available in the paper, respond with: "Insufficient information avaible" instead of guessing.
+Ensure the summary is clear, accurate, and aligned with the provided style and length.
+""",
+input_variables=['paper_input', 'style_input', 'length_input']
+)
+
+#fill the placeholders
+
+prompt = template.invoke({
+    'paper_input': paper_input,
+    'style_input': style_input,
+    'length_input': length_input
+})
+
+if st.button('Summarize'):
+    response = llm.invoke(prompt)
+    st.text(response.content)
+
+# template = load_prompt('template.json')
+
+
+
+# if st.button('Summarize'):
+#     chain = template | llm
+#     result = chain.invoke({
+#         'paper_input':paper_input,
+#         'style_input':style_input,
+#         'length_input':length_input
+#     })
+#     st.write(result.content)
