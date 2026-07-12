cebb8dba30b480298fb0af1032af663a6fd0a28b
bhuvaneshwarguttula <bhuvaneshwarguttula@gmail.com>
2026-07-12 16:23:11 +0530

    Structured Outputs

---
+++ LangChain_Structured_Outputs/with_structured_output_typeddict.py
@@ -0,0 +1,55 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+
+load_dotenv()
+
+# Initialize the Gemini model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+)
+
+# schema
+class Review(TypedDict):
+
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+    name: Annotated[Optional[str], "Write the name of the reviewer"]
+    
+
+structured_model = model.with_structured_output(Review)
+
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by BG
+""")
+print(result)
+print(result['name'])
+
+"""
+{'key_themes': ['performance', 'camera', 'battery life', 'S-Pen', 'design', 'software', 
+'price'], 'summary': 'The Samsung Galaxy S24 Ultra is a high-performance smartphone praised 
+for its powerful Snapdragon 8 Gen 3 processor, long-lasting 5000mAh battery with 45W fast charging, 
+and exceptional 200MP camera with stunning night mode and 100x zoom capabilities. 
+The S-Pen integration is also a valued feature. However, its large size makes one-handed 
+use uncomfortable, and the One UI still includes bloatware. The $1,300 price tag is also a significant drawback, 
+and camera zoom quality degrades beyond 30x.', 'sentiment': 'pos', 'pros': ['Insanely powerful processor 
+(great for gaming and productivity)', 'Stunning 200MP camera with incredible zoom 
+capabilities', 'Long battery life with fast charging', 'S-Pen support is unique 
+and useful'], 'cons': ['Weight and size make it uncomfortable for one-handed use', 
+'Samsung’s One UI comes with bloatware', 'High $1,300 price tag', 'Zoom quality degrades beyond 30x'], 
+'name': 'BG'}
+"""
