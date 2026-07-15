433b67a3a17f8597de2e3fcd64361a4e1ad2ecfb
bhuvaneshwarguttula <bhuvaneshwarguttula@gmail.com>
2026-07-15 21:44:59 +0530

    LangChain Runnables

---
+++ LangChain_Runnables/runnable_sequence.py
@@ -0,0 +1,32 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+import os
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv('HUGGINGFACEHUB_ACCESS_TOKEN')
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
